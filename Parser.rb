e566ddc18c8c406bbf3aba706689630599ee416d
CS31IvanK <144357899+CS31IvanK@users.noreply.github.com>
2023-11-12 11:20:10 +0100

    Update Parser.rb

--- Parser.rb
+++ Parser.rb
@@ -6,7 +6,7 @@ doc = Nokogiri::HTML(URI.open('https://www.hospitalsafetygrade.org/all-hospitals
 hospitals = doc.css('div#BlinkDBContent_849210 li a')
 CSV.open("hospitals.csv", "w") do |csv|
   csv<<["ID", "Hospital", "URL"]
-  i=0
+  i=-1
   hospitals.each do |hospital|
     i+=1
     name = hospital.text
