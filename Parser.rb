a0f753b2c2d6a5d4315737e458118f393ca74db8
CS31IvanK <144357899+CS31IvanK@users.noreply.github.com>
2023-11-14 10:05:53 +0100

    Update Parser.rb

--- Parser.rb
+++ Parser.rb
@@ -5,15 +5,15 @@ require 'open-uri'
 doc = Nokogiri::HTML(URI.open('https://www.hospitalsafetygrade.org/all-hospitals'))
 hospitals = doc.css('div#BlinkDBContent_849210 li a')
 CSV.open("hospitals.csv", "wb") do |csv|
-  csv<<["ID", "Hospital", "URL", "Location"]
+  csv<<["ID", "Hospital", "URL"]#, "Location"]
   i=-1
   hospitals.each do |hospital|
     i+=1
     name = hospital.text
     url=hospital['href']
-    docloc=Nokogiri::HTML(URI.open(url))
-    location = docloc.css('div.address').inner_html.split('<br>')[0..1].join()
-    puts location
-    csv <<[i, name, url, location]
+    #docloc=Nokogiri::HTML(URI.open(url))
+    #location = docloc.css('div.address').inner_html.split('<br>')[0..1].join()
+    #puts location
+    csv <<[i, name, url]#, location]
   end
 end
